e3fe15898258f470323a91d0de430cd1450b6fb9
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-08-05 15:44:13 -0400

    Update Default App Template
    - Updates Dependencies
    - Adds Favicons
    - Replaces Default Metadata

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Alfred AI ",
+  description: "Your personal assistant in your pocket",
   navItems: [
     {
       label: "Home",
