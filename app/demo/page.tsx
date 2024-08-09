e23f1f064d681d2341fe750a412707aeed14657c
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-08-09 04:31:37 -0700

    Add feature for multiple chat sessions: - Implement ability to initiate multiple chat sessions - Enable users to switch between different chat sessions without losing history - Maintain separate conversation histories for each session - Add functionality to add new chat sessions - Update UI to support multiple chats and easy navigation
    Pending:
    - Implement functionality to delete chat sessions

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,11 +1,145 @@
+'use client'
 import { title } from "@/components/primitives";
 import GeminiDemo from "@/components/GeminiDemo";
+import {Tabs, Tab, Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure} from "@nextui-org/react";
+import { useState } from "react";
+
+interface Message {
+  id: string;
+  role: string;
+  content: string;
+  createdAt: string | Date | undefined;
+}
+
+// interface Chat {
+//   name: string;
+//   multiMessages: Message[];
+// }
+
 
 export default function DocsPage() {
+  const [chats, setChats] = useState<{ name: string; multiMessages: Message[]; }[]>([]);
+  const [chatName, setChatName] = useState('');
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+
+  function addChat() {
+    console.log("addChat()");
+    const newChat = { name: chatName, multiMessages: [] as Message[] };
+    const updatedChats = [...chats, newChat];
+    
+    setChats(updatedChats);
+
+
+    setChatName("");
+    console.log("newChat: " + JSON.stringify(newChat));
+    console.log("updatedChats: " + JSON.stringify(updatedChats));
+
+  }
+
+
   return (
-    <div>
+    <div
+      className="flex flex-col gap-4"
+    >
       <h1 className={title()}>Docs</h1>
-      <GeminiDemo />
+      <Modal 
+        backdrop="opaque" 
+        isOpen={isOpen} 
+        onOpenChange={onOpenChange}
+        classNames={{
+          backdrop: "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20"
+        }}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+              <ModalBody>
+                <input
+                  type="text"
+                  value={chatName}
+                  onChange={(e) => {
+                    setChatName(e.target.value);
+                    console.log(chatName);
+                  }}
+                  className="border p-2 mb-4 w-full"
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button
+                  color="primary"
+                  onPress={() => {
+                    console.log("onPress")
+                    addChat();
+                    console.log('chats: ' + JSON.stringify(chats));
+                    onClose();
+                  }}>
+                  Add
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <Button
+        onClick={onOpen}
+      >Add Chat</Button>
+      {
+        chats.length > 0 && (
+          <div className="flex flex-col px-4">
+            <div className="flex w-full flex-col">
+              <Tabs aria-label="Options" placement='start'>
+                {/* {Object.entries(chats ?? {}).map(([key, chat], index) => (
+                  <Tab key={key} title={key}>
+                    <GeminiDemo chatName={key} multiMessages={chat as Message[]} setMultiMessages={(messages: any[]) => {
+                      const updatedChats: { name: string; multiMessages: Message[]; } = {
+                        ...chats,
+                        [key]: messages,
+                      };
+                      setChats(updatedChats);
+                    }} />
+                  </Tab>
+                ))} */}
+                
+                {chats.map((chat, index) => (
+                  <Tab key={index} title={chat.name}>
+                    <GeminiDemo
+                      chatName={chat.name}
+                      multiMessages={chat.multiMessages}
+                      setMultiMessages={(messages: Message[]) => {
+                        const updatedChat = {
+                          ...chat,
+                          multiMessages: messages,
+                        };
+                        const updatedChats = chats.map((c, i) => (i === index ? updatedChat : c));
+                        setChats(updatedChats);
+                      }}
+                    />
+                  </Tab>
+                ))}
+                {/* <Tab key="photos" title="Photos">
+                  <p>A</p>
+                  <GeminiDemo />
+                </Tab>
+                <Tab key="music" title="Music">
+                  <p>B</p>
+                  <GeminiDemo />
+                </Tab>
+                <Tab key="videos" title="Videos">
+                  <p>C</p>
+                  <GeminiDemo />
+                </Tab> */}
+              </Tabs>
+            </div>
+          </div>
+        )
+      }
+      
+      {/* <GeminiDemo /> */}
     </div>
   );
 }
--- components/GeminiDemo.tsx
+++ components/GeminiDemo.tsx
@@ -1,44 +1,72 @@
 'use client'
 import { useChat } from 'ai/react';
+import { title } from "@/components/primitives";
+import { useEffect } from 'react';
 
-const GeminiDemo: React.FC = () => {
+interface Message {
+  id: string;
+  role: string;
+  content: string;
+  createdAt: string | Date | undefined;
+}
+
+// Define an interface for your props
+interface MyComponentProps {
+  chatName: string;
+  multiMessages: Message[];
+  setMultiMessages: (messages: Message[]) => void;
+}
+
+
+const GeminiDemo: React.FC<MyComponentProps> = ({chatName, multiMessages, setMultiMessages}) => {
   const { messages, input, handleInputChange, handleSubmit, isLoading, error } = useChat({
     api: '/api/chat',
-    initialMessages: [],
+    initialMessages: multiMessages as [],
   });
 
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Gemini Integration</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={input}
-          onChange={handleInputChange}
-          className="border p-2 mb-4 w-full"
-          placeholder="Enter your prompt"
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white p-2 rounded"
-          disabled={isLoading}
-        >
-          {isLoading ? 'Generating...' : 'Submit'}
-        </button>
-      </form>
-      {error && <p className="text-red-500 mt-4">{error.message}</p>}
-      {messages.length != 0 && (
-        <div className="mt-4 p-4 border rounded bg">
-            {messages.map(m => (
-              <pre key={m.id} className="whitespace-pre-wrap break-words max-w-full">
-                {m.role === 'user' ? 'User: ' : 'AI: '}
-                {m.content}
-              </pre>
-            ))}
-        </div>
-      )}
 
-    </div>
+  useEffect(() => {
+    console.log("inside GeminiDemo useEffect");
+    // when see a change in messages, update the parent component's state
+    setMultiMessages(messages as Message[]);
+  }, [messages]);
+
+  useEffect(() => {
+    console.log("messages when mounted: " + JSON.stringify(messages));
+  }, [messages]);
+
+  return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">{chatName}</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={input}
+            onChange={handleInputChange}
+            className="border p-2 mb-4 w-full"
+            placeholder="Enter your prompt"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-2 rounded"
+            disabled={isLoading}
+          >
+            {isLoading ? 'Generating...' : 'Submit'}
+          </button>
+        </form>
+        {error && <p className="text-red-500 mt-4">{error.message}</p>}
+        {messages.length != 0 && (
+          <div className="mt-4 p-4 border rounded bg">
+              {messages.map(m => (
+                <pre key={m.id} className="whitespace-pre-wrap break-words max-w-full">
+                  {m.role === 'user' ? 'User: ' : 'AI: '}
+                  {m.content}
+                </pre>
+              ))}
+          </div>
+        )}
+
+      </div>
   );
 };
 
