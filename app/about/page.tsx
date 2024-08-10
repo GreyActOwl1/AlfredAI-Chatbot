66c9e39acf67dfa52cdbaa79dc996263eae675ef
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-08-10 22:08:56 +0500

    updated about page styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,9 +15,9 @@ import { FaLinkedin } from "react-icons/fa";
 
 export default function AboutPage() {
   return (
-    <div>
+    <div className="min-h-screen w-full pb-20 bg-white dark:bg-black">
       <h1 className="pt-12 pb-20 text-black dark:text-white font-semibold text-3xl md:text-4xl text-center">Meet Our Team</h1>
-      <div className="w-full p-2 md:p-0 md:max-w-7xl mx-auto grid grid-cols-1 gap-y-8 md:grid-cols-2">
+      <div className="w-full p-2 md:p-0 md:max-w-7xl mx-auto grid grid-cols-1 bg-white dark:bg-black gap-y-8 md:grid-cols-2 ">
         {/* Mapping TeamDetails Array*/}
         { teamDetails.map((item,index) => (
  <Link href ={item.LinkedinUrl} target="_blank" key={index} className=" p-4 md:p-0 flex flex-col items-center w-full md:w-[80%] h-auto md:h-80 space-y-4 hover:bg-gray-200 bg-gray-100 rounded-md">
