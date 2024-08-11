2b98198af17f1144cccfa817ed4b83e38e7ec1ed
Adrian Ea <106501057+GreyActOwl1@users.noreply.github.com>
2024-08-10 21:30:39 -0400

    Add Appwrite pkg & config

---
+++ appwrite.ts
@@ -0,0 +1,10 @@
+import { Client, Account, OAuthProvider, AppwriteException } from "appwrite";
+
+const client = new Client();
+
+client
+	.setEndpoint(process.env.APPWRITE_API_ENDPONT!)
+	.setProject(process.env.APPWRITE_PROJECT_ID!);
+
+export const account = new Account(client);
+export { AppwriteException, OAuthProvider };
